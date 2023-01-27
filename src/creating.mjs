12d8492351796e4217a99334919ba1b441597ed2
Alan Nunez <raul.nunez01@gmail.com>
2023-01-27 13:59:16 -0400

    Basic changes

--- src/creating.mjs
+++ src/creating.mjs
@@ -3,13 +3,47 @@ import setText, { appendText } from "./results.mjs";
 export function timeout(){
 }
 
-export function interval(){
+export function interval() {
+  let counter = 0;
+  const wait = new Promise((resolve) => {
+    setInterval(() => {
+      console.log("INTERVAL");
+      resolve(`Timeout ${++counter}`);
+    }, 1500);
+  });
+  wait.then((text) => setText(text));
 }
 
-export function clearIntervalChain(){
+export function clearIntervalChain() {
+    let counter = 0;
+    let interval = 0 ;
+    const wait = new Promise((resolve) => {
+      interval = setInterval(() => {
+        console.log("INTERVAL");
+        resolve(`Timeout ${++counter}`);
+      }, 1500);
+    });
+    wait.then((text) => {
+        clearInterval(interval);
+        setText(text);
+    });
 }
 
-export function xhr(){
+export function xhr() {
+    let request = new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:3000/users/7");
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          resolve(xhr.responseText);
+        } else {
+          reject(xhr.statusText);
+        }
+      };
+      xhr.send();
+    });
+
+    request.then((result) => setText(result));
 }
 
 export function allPromises(){
